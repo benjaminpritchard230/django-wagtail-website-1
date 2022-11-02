e8204104f3e484c280c384e05221c07aa7507f86
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2022-11-02 20:31:46 +0000

    Making a wagtail website

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-ye=)-^z^jnvf3^zz@e8-9!-%92v@+)n2kuqq^0rsva_fek3&7g"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
